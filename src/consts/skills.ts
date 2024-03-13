4a200b9b7ffd72db9191b3c35cd3a10e5e4fddc8
ooooose <yuuki.oose1990@gmail.com>
2024-03-13 15:08:58 +0900

    add socialLinksの追加

---
+++ src/consts/skills.ts
@@ -0,0 +1,149 @@
+export const skills = [
+  {
+    name: "C++",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/c-plusplus.svg",
+  },
+  {
+    name: "Rust",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/rust.svg",
+  },
+  {
+    name: "JavaScript",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/javascript.svg",
+  },
+  {
+    name: "TypeScript",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/typescript-icon.svg",
+  },
+  {
+    name: "Python",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/python.svg",
+  },
+  {
+    name: "Go",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/go.svg",
+  },
+  {
+    name: "FastAPI",
+    level: "Advanced",
+    icon: "https://cdn.worldvectorlogo.com/logos/fastapi-1.svg",
+  },
+  {
+    name: "React",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/react.svg",
+  },
+  {
+    name: "Next.js",
+    level: "Advanced",
+    icon: "https://cdn.svgporn.com/logos/nextjs.svg",
+  },
+  {
+    name: "echo",
+    level: "Advanced",
+    icon: "https://camo.githubusercontent.com/7079c0f1404be1ddf9627e3672faa46f77cd720f0569e3e2748aa3c7849b52d0/68747470733a2f2f63646e2e6c6162737461636b2e636f6d2f696d616765732f6563686f2d6c6f676f2e737667",
+  },
+  {
+    name: "GCP",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/google-cloud.svg",
+  },
+
+  {
+    name: "GraphQL",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/graphql.svg",
+  },
+  {
+    name: "Docker",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/docker-icon.svg",
+  },
+  {
+    name: "AWS",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/aws.svg",
+  },
+
+  {
+    name: "Firebase",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/firebase.svg",
+  },
+  {
+    name: "MySQL",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/mysql.svg",
+  },
+  {
+    name: "PostgreSQL",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/postgresql.svg",
+  },
+  {
+    name: "MongoDB",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/mongodb.svg",
+  },
+  {
+    name: "Redis",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/redis.svg",
+  },
+  {
+    name: "Elasticsearch",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/elasticsearch.svg",
+  },
+  {
+    name: "Kubernetes",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/kubernetes.svg",
+  },
+  {
+    name: "Terraform",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/terraform-icon.svg",
+  },
+  {
+    name: "Ansible",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/ansible.svg",
+  },
+  {
+    name: "CircleCI",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/circleci.svg",
+  },
+  {
+    name: "GitHub Actions",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/github-icon.svg",
+  },
+  {
+    name: "Jenkins",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/jenkins.svg",
+  },
+  {
+    name: "Apache",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/apache.svg",
+  },
+  {
+    name: "Nginx",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/nginx.svg",
+  },
+  {
+    name: "Vim",
+    level: "Intermediate",
+    icon: "https://cdn.svgporn.com/logos/vim.svg",
+  },
+];
