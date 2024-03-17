43849359286412a726d0578e4f5c8a7f341dc202
ooooose <yuuki.oose1990@gmail.com>
2024-03-17 23:58:13 +0900

    Fix レイアウト刷新

--- src/consts/url.ts
+++
@@ -1,6 +0,0 @@
-export const TWITTER_URL = "https://twitter.com/ooooonse0524";
-export const GITHUB_URL = "https://github.com/ooooose";
-export const NOTE_URL = "https://note.com/ooooonse0524";
-export const X_URL = "https://twitter.com/ooooonse0524";
-export const ZENN_URL = "https://zenn.dev/ooooose";
-export const QIITA_URL = "https://qiita.com/yuuki_0524";
